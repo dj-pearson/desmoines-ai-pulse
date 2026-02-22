8788e9a90813467050cb179a161f6f960e5fd13e
Dj Pearson <pearson.performance@gmail.com>
2026-02-22 10:45:41 -0600

    feat: add useNavigate import to useKeyboardShortcuts hook

--- src/hooks/use-keyboard-shortcuts.ts
+++ src/hooks/use-keyboard-shortcuts.ts
@@ -1,4 +1,5 @@
 import { useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { createLogger } from '@/lib/logger';
 
 const log = createLogger('useKeyboardShortcuts');
